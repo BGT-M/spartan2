afb1e7a87572ebe749aaa17bf3b57af8d05508ac
tdingquan <tdingquan@gmail.com>
2020-04-09 22:03:59 +0800

    update setup.py, add required_packages

--- setup.py
+++ setup.py
@@ -1,9 +1,29 @@
-from setuptools  import setup
+from setuptools import setup
 
-setup(name='spartan2',
-      version=0,
-      description='collection of data mining algorithms on big graphs and time series',
-      author='Shenghua Liu',
-      author_email='liu.shengh@foxmail.com',
+NAME = 'spartan2'
+VERSION = 0
+DESCRIPTION = 'collection of data mining algorithms on big graphs and time series'
+URL = 'https://github.com/shenghua-liu/spartan2'
+AUTHOR = 'Shenghua Liu, Houquan Zhou, Quan Ding'
+EMAIL = 'liu.shengh@foxmail.com'
+
+REQUIRED_PACKAGES = [
+      'numpy',
+      'scipy',
+      'networkx',
+      'matplotlib',
+      'statsmodels',
+      'pomegranate',
+      'scikit-learn',
+      'scikit-image',
+]
+
+setup(name=NAME,
+      version=VERSION,
+      description=DESCRIPTION,
+      author=AUTHOR,
+      author_email=EMAIL,
+      url=URL,
+      install_requires=REQUIRED_PACKAGES,
       packages=['spartan2', 'spartan2.algorithm', 'spartan2.models', 'spartan2.tensor'],
       )
