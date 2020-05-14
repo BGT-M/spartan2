b53daf4541dd304a68c8e34562dbe2f7748e9f13
tdingquan <tdingquan@gmail.com>
2020-05-15 00:22:07 +0800

    add read file format judgement

--- spartan2/tensor/STTensor.py
+++ spartan2/tensor/STTensor.py
@@ -2,25 +2,33 @@
 # -*- coding:utf-8 -*-
 # Authors: Shenghua Liu
 
-from .tensor.STTimeseries import STTimeseries
+from .STTimeseries import STTimeseries
+from .STGraph import STGraph
+from .ioutil import checkfileformat
 
 import os
 import sys
 import numpy as np
 import pandas as pd
-from scipy.sparse import csc_matrix, coo_matrix, csr_matrix, lil_matrix
+from scipy.sparse import coo_matrix
 
-import matplotlib.pyplot as plt
-import matplotlib
-matplotlib.rcParams['font.sans-serif'] = ['SimHei']
-matplotlib.rcParams['axes.unicode_minus'] = False
-plt.rcParams['savefig.dpi'] = 200
+'''Input tensor format:
+    format: att1, att2, ..., value1, value2, ...
+    comment line started with #
+    e.g.
+    user obj 1
+    ... ...
+    if multivariate time series, hasvalue equals to the number of
+    time series
+    return: tensorlist
+'''
 
 
 class STTensor:
-    def __init__(self, tensorlist, hasvalue):
+    def __init__(self, tensorlist, hasvalue, value_idx):
         self.tensorlist = tensorlist
         self.hasvalue = hasvalue
+        self.value_idx = value_idx
         'number of columns'
         self.m = len(tensorlist[0])
 
@@ -87,9 +95,12 @@ class STTensor:
         time = []
         start = 0
         attrlists = np.array(self.tensorlist).T
+        if self.value_idx is None:
+            self.value_idx = 0
         if self.hasvalue == True:
-            time = attrlists[0]
-            start = 1
+            time = attrlists[self.value_idx]
+            del attrlists[self.value_idx]
+            start = 0
         if numsensors is None:
             tensors = attrlists[start:]
         else:
@@ -99,24 +110,41 @@ class STTensor:
         return STTimeseries(time, attrlists, attrlabels, freq=freq, startts=startts)
 
 
-class STGraph:
-    def __init__(self, sm, weighted, bipartite, rich=False, attlist=None, relabel=False, labelmaps=(None, None)):
-        '''
-            sm: sparse adj matrix of (weighted) graph
-            weighted: graph is weighte or not
-            attlist: attribute list with edges, no values
-            relabel: relabel or not
-            labelmaps: label maps from old to new, and inverse maps from new to
-            old
-        '''
-        self.sm = sm
-        self.weighted = weighted
-        self.rich = rich
-        self.attlist = attlist
-        self.relabel = relabel
-        self.labelmaps = labelmaps
-        self.bipartite = bipartite
+def loadTensor(name: str, path: str, col_idx: list = None, col_types: list = None,
+               hasvalue: bool = 1, value_idx: int = None) -> STTensor:
+    '''Load tensor from file.
 
-    def degrees(self):
-        rowdegs, coldegs = self.sm.sum(axis=1), self.sm.sum(axis=0)
-        return rowdegs, coldegs.T
+    Args:
+        name: file name
+        path: relative or absolute path of directory
+        col_idx: id of chosen columns in data file
+        col_types: data type of each chosen column
+        hasvalue:
+            for time series data, this refers to if time column exists
+            for graph data, this refers to 
+        value_idx: id of value column
+
+    Returns:
+        STTensor object
+    '''
+    if path == None:
+        path = "inputData/"
+    full_path = os.path.join(path, name)
+    if col_idx is None:
+        col_idx = [i for i in range(len(col_types))]
+    if len(col_idx) == len(col_types):
+        idxtypes = [(col_idx[i], col_types[i]) for i in range(len(col_idx))]
+    else:
+        raise Exception(f"Error: input same size of col_types and col_idx")
+    tensorlist = checkfileformat(full_path + '.tensor', idxtypes)
+    printTensorInfo(tensorlist, hasvalue)
+    return STTensor(tensorlist, hasvalue, value_idx)
+
+
+def printTensorInfo(tensorlist, hasvalue):
+    m = len(tensorlist[0]) - hasvalue
+    print(f"Info: Tensor is loaded\n\
+           ----------------------\n\
+             attr     |\t{m}\n\
+             values   |\t{hasvalue}\n\
+             nonzeros |\t{len(tensorlist)}\n")
