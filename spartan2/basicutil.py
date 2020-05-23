fc5cbbfea855dc5b492b525b2f8b370be110d627
张嘉宝 <771089057@qq.com>
2020-05-23 18:26:53 +0800

    simplify IAT class

--- spartan2/basicutil.py
+++ spartan2/basicutil.py
@@ -1,6 +1,7 @@
 import numpy as np
 import spartan2.ioutil as ioutil
 
+
 class IAT:
     aggiat = {}  # key:user; value:iat list
     aggiatpair = {}  # key:user; value: (iat1, iat2) list
@@ -16,18 +17,18 @@ class IAT:
     def calaggiat(self, aggts):
         'aggts: key->user; value->timestamp list'
         for k, lst in aggts.items():
-            if len(lst)<2:
+            if len(lst) < 2:
                 continue
             lst.sort()
             iat = np.diff(lst)
-            self.aggiat[k]= iat
+            self.aggiat[k] = iat
 
     def save_aggiat(self, outfile):
         ioutil.saveDictListData(self.aggiat, outfile)
 
     def load_aggiat(self, infile):
         self.aggiat = ioutil.loadDictListData(infile, ktype=str, vtype=int)
-        
+
     def calaggiatpair(self):
         for k, lst in self.aggiat.items():
             pairs = []
@@ -35,19 +36,13 @@ class IAT:
                 pair = (lst[i], lst[i + 1])
                 pairs.append(pair)
             self.aggiatpair[k] = pairs
-    
-    def save_aggiatpair(self, outfile):
-        ioutil.saveDictListData(self.aggiatpair, outfile)
 
-    def load_aggiatpair(self, infile):
-        self.aggiatpair = ioutil.loadDictListData(infile, ktype=str, vtype=int)
-    
     def getiatpairs(self):
         xs, ys = [], []
         for k, lst in self.aggiat.items():
-            for i in range(len(lst)-1):
+            for i in range(len(lst) - 1):
                 xs.append(lst[i])
-                ys.append(lst[i+1])
+                ys.append(lst[i + 1])
         return xs, ys
 
     def caliatcount(self):
@@ -56,26 +51,19 @@ class IAT:
                 if iat not in self.iatcount:
                     self.iatcount[iat] = 0
                 self.iatcount[iat] += 1
-        
+
     def caliatpaircount(self):
         for k, lst in self.aggiat.items():
-            if len(lst) >= 2:
-                iat1 = lst[0]
-                for iat2 in lst[1:]:
-                    iatpair = (iat1, iat2)
-                    iat1 = iat2
-                    if iatpair not in self.iatcount:
-                        self.iatpaircount[iatpair] = 0
-                    self.iatpaircount[iatpair] += 1
-            else:
-                continue
-    
+            for i in range(len(lst) - 1):
+                pair = (lst[i], lst[i+1])
+                if pair not in self.iatcount:
+                    self.iatpaircount[pair] = 0
+                self.iatpaircount[pair] += 1
+
     'find users that have pairs in iatpairs'
     def find_iatpair_user(self, iatpairs):
         usrlist, pairset = [], set(iatpairs)
         for k, lst in self.aggiatpair.items():
-            if len(set(lst) & pairset) == 0:
-                continue
-            else:
+            if len(set(lst) & pairset) != 0:
                 usrlist.append(k)
         return usrlist
