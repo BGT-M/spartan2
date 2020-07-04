5d1bb9a546d66928df4d3e458aeb5b547b88f2c4
tdingquan <tdingquan@gmail.com>
2020-07-04 15:01:08 +0800

    update beatlex demo

--- spartan/tensor/tensor.py
+++ spartan/tensor/tensor.py
@@ -10,3 +10,13 @@
 
 class STTensor:
     pass
+
+
+#TODO => __str__ of STTensor
+def printTensorInfo(tensorlist, hasvalue):
+    m = len(tensorlist[0]) - hasvalue
+    print(f"Info: Tensor is loaded\n\
+           ----------------------\n\
+             attr     |\t{m}\n\
+             values   |\t{hasvalue}\n\
+             nonzeros |\t{len(tensorlist)}\n")
