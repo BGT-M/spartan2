0771c6eb349f12d7797d27300d35efb1ad7de759
shenghua-liu <liu.shengh@foxmail.com>
2020-07-14 15:29:27 +0800

    remove load2

--- spartan/util/__init__.py
+++ spartan/util/__init__.py
@@ -1,11 +1,10 @@
 from spartan.tensor import TensorData
 
-from .ioutil import loadTensor, loadTensor2
+from .ioutil import loadTensor
 
 MODEL_PATH = 'spartan.model'
 
 __all__ = [
     'MODEL_PATH',
-    'loadTensor',
-    'loadTensor2'
+    'loadTensor'
 ]
