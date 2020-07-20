bf043305873e6901942f8af05f72d21a37dbc9e6
tdingquan <tdingquan@gmail.com>
2020-07-20 15:10:23 +0800

    add dynamic import to model level

--- spartan/model/__init__.py
+++ spartan/model/__init__.py
@@ -1,4 +1,15 @@
-from .beatlex.Beatlex import BeatLex
+from spartan.util import MODEL_PATH
+from enum import Enum
+from functools import partial
+
+
+def __call__(policy, *args, **kwargs):
+    import importlib
+    model_cls = importlib.import_module(policy).__call__()
+    return model_cls(*args, **kwargs)
+
+
+BeatLex = partial(__call__, MODEL_PATH + ".beatlex")
 
 __all__ = [
     'BeatLex'
