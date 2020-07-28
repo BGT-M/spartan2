83949c7f24d20cfa57334885cf06e21f04f048ba
tdingquan <tdingquan@gmail.com>
2020-07-28 11:40:00 +0800

    Update io. Ref: #37

--- spartan/util/ioutil.py
+++ spartan/util/ioutil.py
@@ -22,6 +22,7 @@ class File():
         self.mode = mode
         self.idxtypes = idxtypes
         self.dtypes = None
+        self.sep = None
 
     def get_sep_of_file(self):
         '''
@@ -29,81 +30,23 @@ class File():
         :param infn: input file
         '''
         sep = None
-        with self._open() as fp:
-            for line in fp:
-                line = line.decode(
-                    'utf-8') if isinstance(line, bytes) else line
-                if (line.startswith("%") or line.startswith("#")):
-                    continue
-                line = line.strip()
-                if (" " in line):
-                    sep = " "
-                if ("," in line):
-                    sep = ","
-                if (";" in line):
-                    sep = ';'
-                if ("\t" in line):
-                    sep = "\t"
-                break
+        fp = open(self.filename, self.mode)
+        for line in fp:
+            line = line.decode(
+                'utf-8') if isinstance(line, bytes) else line
+            if (line.startswith("%") or line.startswith("#")):
+                continue
+            line = line.strip()
+            if (" " in line):
+                sep = " "
+            if ("," in line):
+                sep = ","
+            if (";" in line):
+                sep = ';'
+            if ("\t" in line):
+                sep = "\t"
+            break
         self.sep = sep
-
-    def _open(self):
-        pass
-
-    def _read(self):
-        pass
-
-
-class TensorFile(File):
-    def _open(self):
-        if 'r' not in self.mode:
-            self.mode += 'r'
-        f = open(self.filename, self.mode)
-        return f
-
-    def _read(self):
-        tensorlist = []
-        self.get_sep_of_file()
-        with self._open() as fin:
-            for line in fin:
-                line = line.strip()
-                if line.startswith("#"):
-                    continue
-                coords = line.split(self.sep)
-                tline = []
-                for i, tp in self.idxtypes:
-                    try:
-                        tline.append(tp(coords[i]))
-                    except Exception:
-                        raise Exception(
-                            f"The {i}-th col does not match the given type {tp} in line:\n{line}")
-                tensorlist.append(tline)
-        tensorlist = pd.DataFrame(tensorlist)
-        return tensorlist
-
-
-class CSVFile(File):
-    def _open(self):
-        f = pd.read_csv(self.filename, header=None)
-        column_names = f.columns
-        self.dtypes = {}
-        if not self.idxtypes is None:
-            for idx, typex in self.idxtypes:
-                self.dtypes[column_names[idx]] = self.transfer_type(typex)
-            f = pd.read_csv(self.filename, dtype=self.dtypes, header=None)
-        else:
-            f = pd.read_csv(self.filename, header=None)
-        return f
-
-    def _read(self):
-        tensorlist = pd.DataFrame()
-        _file = self._open()
-        if not self.idxtypes is None:
-            idx = [i[0] for i in self.idxtypes]
-            tensorlist = _file[idx]
-        else:
-            tensorlist = _file
-        return tensorlist
 
     def transfer_type(self, typex):
         if typex == float:
@@ -116,8 +59,93 @@ class CSVFile(File):
             _typex = 'object'
         return _typex
 
+    def _open(self, **kwargs):
+        pass
 
-def _read_data(filename: str, idxtypes: list) -> object:
+    def _read(self, **kwargs):
+        pass
+
+
+class TensorFile(File):
+    def _open(self, **kwargs):
+        if 'r' not in self.mode:
+            self.mode += 'r'
+        f = open(self.filename, self.mode)
+        pos = 0
+        cur_line = f.readline()
+        while cur_line.startswith("#"):
+            pos = f.tell()
+            cur_line = f.readline()
+        f.seek(pos)
+        _f = open(self.filename, self.mode)
+        _f.seek(pos)
+        fin = pd.read_csv(f, sep=self.sep, **kwargs)
+        column_names = fin.columns
+        self.dtypes = {}
+        if not self.idxtypes is None:
+            for idx, typex in self.idxtypes:
+                self.dtypes[column_names[idx]] = self.transfer_type(typex)
+            fin = pd.read_csv(_f, dtype=self.dtypes, sep=self.sep, **kwargs)
+        else:
+            fin = pd.read_csv(_f, sep=self.sep, **kwargs)
+        return fin
+
+    def _read(self, **kwargs):
+        tensorlist = []
+        self.get_sep_of_file()
+        tensorlist = self._open(**kwargs)
+        tensorlist = pd.DataFrame(tensorlist)
+        return tensorlist
+
+
+class CSVFile(File):
+    def _open(self, **kwargs):
+        f = pd.read_csv(self.filename, **kwargs)
+        column_names = f.columns
+        self.dtypes = {}
+        if not self.idxtypes is None:
+            for idx, typex in self.idxtypes:
+                self.dtypes[column_names[idx]] = self.transfer_type(typex)
+            f = pd.read_csv(self.filename, dtype=self.dtypes, **kwargs)
+        else:
+            f = pd.read_csv(self.filename, **kwargs)
+        return f
+
+    def _read(self, **kwargs):
+        tensorlist = pd.DataFrame()
+        _file = self._open(**kwargs)
+        if not self.idxtypes is None:
+            idx = [i[0] for i in self.idxtypes]
+            tensorlist = _file[idx]
+        else:
+            tensorlist = _file
+        return tensorlist
+
+
+class NPFile(File):
+    def _open(self, **kwargs):
+        f = np.load(self.filename)
+        return f
+    
+    def _read(self, **kwargs):
+        f = self._open(**kwargs)
+        if self.filename.endswith('.npy'):
+            df = pd.DataFrame(f)
+        elif self.filename.endswith('.npz'):
+            df = pd.DataFrame(f.values())
+        else:
+            df = None
+        if not self.idxtypes is None:
+            idx = [i[0] for i in self.idxtypes]
+            types = [i[1] for i in self.idxtypes]
+            df = df[idx]
+            for i, _type in enumerate(types):
+                df[i] = df[i].astype(self.transfer_type(_type))
+        else:
+            df = df
+        return df
+
+def _read_data(filename: str, idxtypes: list, **kwargs) -> object:
     """Check format of file and read data from file.
 
     Default format is .tensor. Now we support read from csv, gz, tensor.
@@ -142,19 +170,22 @@ def _read_data(filename: str, idxtypes: list) -> object:
     _class = None
     _postfix = os.path.splitext(filename)[-1]
     if _postfix == ".csv":
-        _filename = filename
         _class = CSVFile
     elif _postfix == ".tensor":
-        _filename = filename
         _class = TensorFile
     elif _postfix in ['.gz', '.bz2', '.zip', '.xz']:
-        _filename = filename
         _class = CSVFile
+    elif _postfix in ['.npy', '.npz']:
+        _class = NPFile
     else:
+        for _postfix in [".csv", ".tensor", '.gz', '.bz2', '.zip', '.xz', '.npy', '.npz']:
+            if os.path.isfile(filename + _postfix):
+                _filename = filename + _postfix
+                return _read_data(_filename, idxtypes, **kwargs)
         raise FileNotFoundError(
             f"Error: Can not find file {filename}, please check the file path!\n")
-    _obj = _class(_filename, 'r', idxtypes)
-    _data = _obj._read()
+    _obj = _class(filename, 'r', idxtypes)
+    _data = _obj._read(**kwargs)
     return _data
 
 
@@ -182,7 +213,7 @@ def _aggregate(data_list):
         pass
 
 
-def loadTensor(path: str, col_idx: list = None, col_types: list = None):
+def loadTensor(path: str, col_idx: list = None, col_types: list = None, **kwargs):
     if path is None:
         raise FileNotFoundError('Path is missing.')
     path = _check_compress_file(path)
@@ -203,6 +234,6 @@ def loadTensor(path: str, col_idx: list = None, col_types: list = None):
 
     data_list = []
     for _file in files:
-        data_list.append(_read_data(_file, idxtypes))
+        data_list.append(_read_data(_file, idxtypes, **kwargs))
     data = _aggregate(data_list)
     return TensorData(data)
