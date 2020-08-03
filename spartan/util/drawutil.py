2334932998b696d956ff36005627a81365f31443
HQJo <fuhrer.zhou@gmail.com>
2020-08-03 15:28:27 +0800

    Add bipartite and label support for graph plotting

--- spartan/util/drawutil.py
+++ spartan/util/drawutil.py
@@ -14,20 +14,28 @@ from spartan.tensor import Graph
 # TODO do not import matplotlib function in model file
 
 
-def plot_graph(graph: Graph, layout=None, *args, **kwargs):
+def plot_graph(graph: Graph, layout=None, bipartite=False, labels=None,
+               *args, **kwargs):
     import networkx as nx
+    from networkx.algorithms.bipartite.matrix import from_biadjacency_matrix
     nx_layout = {
-        None: nx.draw_networkx,
-        'circular': nx.draw_circular,
-        'kamada_kawai': nx.draw_kamada_kawai,
-        'random': nx.draw_random,
-        'shell': nx.draw_shell,
-        'spectral': nx.draw_spectral,
-        'spring': nx.draw_spring
+        None: nx.random_layout,
+        'circular': nx.circular_layout,
+        'kamada_kawai': nx.kamada_kawai_layout,
+        'random': nx.random_layout,
+        'shell': nx.shell_layout,
+        'spectral': nx.spectral_layout,
+        'spring': nx.spring_layout,
+        'bipartite': nx.bipartite_layout
     }
-    draw_func = nx_layout[layout]
-    g = nx.from_scipy_sparse_matrix(graph.sm)
-    draw_func(g, *args, **kwargs)
+    if bipartite:
+        g = from_biadjacency_matrix(graph.sm)
+    else:
+        g = nx.from_scipy_sparse_matrix(graph.sm)
+    pos = nx_layout[layout](g)
+    nx.draw_networkx(g, pos=pos)
+    if labels is not None:
+        nx.draw_networkx_labels(g, pos=pos, labels=labels)
 
 
 def drawEigenPulse(densities: list = [], figpath: str = None):
@@ -35,12 +43,12 @@ def drawEigenPulse(densities: list = [], figpath: str = None):
     plt.plot(xs, densities, label='density')
     plt.xlabel('window idx')
     plt.ylabel('density')
-    
+
     thres = np.mean(densities) + 3 * np.std(densities)
-    plt.hlines(thres, min(xs), max(xs), linestyles='dashed', colors='yellow', label='threshold')
+    plt.hlines(thres, min(xs), max(xs), linestyles='dashed',
+               colors='yellow', label='threshold')
     plt.legend()
     if figpath is None:
         plt.show()
     else:
-        plt.savefig(figpath)    
-
+        plt.savefig(figpath)
