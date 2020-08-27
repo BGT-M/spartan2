cc3af8adb0881ebff955197dcc3d6219c76d58d8
tdingquan <tdingquan@gmail.com>
2020-08-27 20:13:09 +0800

    Add cython config to setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,8 @@
 #
 
 import setuptools
+from setuptools.extension import Extension
+from Cython.Build import cythonize
 
 NAME = 'spartan2'
 VERSION = '0.0.2'
@@ -15,35 +17,41 @@ EMAIL = 'liu.shengh@foxmail.com'
 with open('README.md', encoding='utf-8') as fp:
     _LONG_DESCRIPTION = fp.read()
 
+extensions = [
+    Extension(
+        "spartan.model.summarize.c_MDL",
+        ["spartan/model/summarize/c_MDL.pyx"],
+    ),
+]
 
 REQUIRED_PACKAGES = [
-      'numpy',
-      'scipy',
-      'networkx',
-      'matplotlib',
-      'statsmodels',
-      'pomegranate',
-      'scikit-learn',
-      'scikit-image',
-      'sparse',
-      'ipdb',
+    'numpy',
+    'scipy',
+    'networkx',
+    'matplotlib',
+    'statsmodels',
+    'pomegranate',
+    'scikit-learn',
+    'scikit-image',
+    'sparse',
+    'ipdb',
 ]
 
 setuptools.setup(name=NAME,
-      version=VERSION,
-      description=DESCRIPTION,
-      long_description=_LONG_DESCRIPTION,
-      long_description_content_type='text/markdown',
-      author=AUTHOR,
-      author_email=EMAIL,
-      url=URL,
-      install_requires=REQUIRED_PACKAGES,
-      packages=setuptools.find_packages(),
-      classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-      ],
-      python_requires='>=3.6',
-      )
-
+                 version=VERSION,
+                 description=DESCRIPTION,
+                 long_description=_LONG_DESCRIPTION,
+                 long_description_content_type='text/markdown',
+                 author=AUTHOR,
+                 author_email=EMAIL,
+                 url=URL,
+                 install_requires=REQUIRED_PACKAGES,
+                 packages=setuptools.find_packages(),
+                 ext_modules=cythonize(extensions),
+                 classifiers=[
+                     "Programming Language :: Python :: 3",
+                     "License :: OSI Approved :: MIT License",
+                     "Operating System :: OS Independent",
+                 ],
+                 python_requires='>=3.6',
+                 )
