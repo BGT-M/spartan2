b1cc797c81274c25de598850a61215bf5ce5c60b
Zeng Siwei <zengsw_study@qq.com>
2021-05-12 15:47:11 +0800

    add maxsize arg to Fraudar

--- spartan/model/fraudar/Fraudar.py
+++ spartan/model/fraudar/Fraudar.py
@@ -9,15 +9,39 @@ class Fraudar(DMmodel):
     def __init__(self, data_mat):
         self.data = data_mat
 
-    def run(self, out_path = "./", file_name = "out"):
+    def run(self, out_path = "./", file_name = "out", k = 1, maxsize = -1):
         sparse_matrix = self.data.to_scipy()
         sparse_matrix = sparse_matrix.asfptype()
-        res = logWeightedAveDegree(sparse_matrix)
-        print(res)
-        np.savetxt("%s.rows" % (out_path + file_name, ), np.array(list(res[0][0])), fmt='%d')
-        np.savetxt("%s.cols" % (out_path + file_name, ), np.array(list(res[0][1])), fmt='%d')
-        print("score obtained is ", res[1])
-    
+        Mcur = sparse_matrix.copy().tolil()
+        res = []
+        t = 0
+        while (t < k):
+            set_row, set_col, score = logWeightedAveDegree(Mcur)
+            list_row, list_col = list(set_row), list(set_col)
+            print("Fraudar iter %s finished." % t)
+            
+            if isinstance(maxsize, int):
+                if (maxsize==-1 or (maxsize>=len(list_row) and maxsize>=len(list_col))):
+                    t += 1
+                    res.append((list_row, list_col, score))
+            elif maxsize[0]>=len(list_row) and maxsize[1]>=len(list_col):
+                t += 1
+                res.append((list_row, list_col, score))
+
+            np.savetxt("%s_%s.rows" % (out_path + file_name, t), np.array(list_row).reshape(-1, 1), fmt='%d')
+            np.savetxt("%s_%s.cols" % (out_path + file_name, t), np.array(list_col).reshape(-1, 1), fmt='%d')
+            print("score obtained is ", score)
+
+            if (t >= k):
+                break
+
+            (rs, cs) = Mcur.nonzero() # (u, v)
+            ## only delete inner connections
+            rowSet = set(list_row)
+            colSet = set(list_col)
+            for i in range(len(rs)):
+                if rs[i] in rowSet and cs[i] in colSet:
+                    Mcur[rs[i], cs[i]] = 0
     
     def anomaly_detection(self):
         return self.run()
