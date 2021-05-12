611f85280c32814be14c4ba7945d77298656580e
Zeng Siwei <zengsw_study@qq.com>
2021-05-12 15:23:23 +0800

    add max_size arg

--- spartan/model/fraudar/Fraudar.py
+++ spartan/model/fraudar/Fraudar.py
@@ -9,14 +9,36 @@ class Fraudar(DMmodel):
     def __init__(self, data_mat):
         self.data = data_mat
 
-    def run(self, out_path = "./", file_name = "out"):
+    def run(self, out_path = "./", file_name = "out", k = 1, max_size = -1):
         sparse_matrix = self.data.to_scipy()
         sparse_matrix = sparse_matrix.asfptype()
-        res = logWeightedAveDegree(sparse_matrix)
-        print(res)
-        np.savetxt("%s.rows" % (out_path + file_name, ), np.array(list(res[0][0])), fmt='%d')
-        np.savetxt("%s.cols" % (out_path + file_name, ), np.array(list(res[0][1])), fmt='%d')
-        print("score obtained is ", res[1])
+        Mcur = sparse_matrix.copy().tolil()
+        res = []
+ 
+        while (t < k):
+            list_row, list_col, score = logWeightedAveDegree(Mcur)
+            print("Fraudar iter %s finished." % t)
+            
+            if isinstance(max_size, int):
+                if (max_size==-1 or (max_size>=len(list_row) and max_size>=len(list_col))):
+                    t += 1
+                    res.append((list_row, list_col, score))
+            elif max_size[0]>=len(list_row) and max_size[1]>=len(list_col):
+                t += 1
+                res.append((list_row, list_col, score))
+            if (t >= k) break
+
+            (rs, cs) = Mcur.nonzero() # (u, v)
+            ## only delete inner connections
+            rowSet = set(list_row)
+            colSet = set(list_col)
+            for i in range(len(rs)):
+                if rs[i] in rowSet and cs[i] in colSet:
+                    Mcur[rs[i], cs[i]] = 0
+
+            np.savetxt("%s_%s.rows" % (out_path + file_name, t), np.array(list_row), fmt='%d')
+            np.savetxt("%s_%s.cols" % (out_path + file_name, t), np.array(list_col), fmt='%d')
+            print("score obtained is ", res[1])
     
     
     def anomaly_detection(self):
